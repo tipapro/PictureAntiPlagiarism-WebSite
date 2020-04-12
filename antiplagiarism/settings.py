5a8adcb8d0dd5428f368840d2dce4bad7dfd904d
Alexandr <48125109+adropla@users.noreply.github.com>
2020-04-12 23:39:17 +0300

    Update settings.py

--- antiplagiarism/settings.py
+++ antiplagiarism/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
