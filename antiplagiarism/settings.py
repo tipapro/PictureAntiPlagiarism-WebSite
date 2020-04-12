d520e08a620e81fc8e0927bd99c73eaea252aa0e
Alexandr <48125109+adropla@users.noreply.github.com>
2020-04-12 23:24:24 +0300

    Update settings.py

--- antiplagiarism/settings.py
+++ antiplagiarism/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'antiplagiarism/static')
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
