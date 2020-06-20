9f7bcb57c053f8a135e2225bd26e9cc5fea74839
Andrey Bogomolov <tipapro@mail.ru>
2020-06-20 11:56:55 +0300

    Update image_iterator.py

--- search_plagiarism/search_module/image_iterator.py
+++ search_plagiarism/search_module/image_iterator.py
@@ -5,36 +5,36 @@ class ImageIterator:
     __chunk_size__ = 100
 
     def __init__(self, database_url):
-        self.__data_provider__ = ImageDataProvider(database_url)
-        self.__data_provider__.connect()
-        self.count = self.__data_provider__.count()
+        self.__data_provider = ImageDataProvider(database_url)
+        self.__data_provider.connect()
+        self.count = self.__data_provider.count()
 
     def __iter__(self):
         print(f'Reset iterator')
-        self.__cur_pos__ = 0
-        self.__cur_arr_pos__ = 0
-        self.__cur_arr_id__, self.__cur_arr_vec__ = self.__data_provider__.get(0, self.__chunk_size__)
+        self.__cur_pos = 0
+        self.__cur_arr_pos = 0
+        self.__cur_arr_id, self.__cur_arr_vec = self.__data_provider.get(0, self.__chunk_size)
         return self
 
     def __next__(self):
         print(f'Current pos {self.__cur_pos__}')
-        if self.__cur_pos__ < self.count:
-            if self.__cur_arr_pos__ < self.__chunk_size__:
-                image_ids, arr = self.__cur_arr_id__[self.__cur_arr_pos__], self.__cur_arr_vec__[self.__cur_arr_pos__]
-                self.__cur_arr_pos__ += 1
-                self.__cur_pos__ += 1
+        if self.__cur_pos < self.count:
+            if self.__cur_arr_pos < self.__chunk_size:
+                image_ids, arr = self.__cur_arr_id[self.__cur_arr_pos], self.__cur_arr_vec[self.__cur_arr_pos]
+                self.__cur_arr_pos += 1
+                self.__cur_pos += 1
                 return image_ids, arr
             else:
-                self.__download_next_arrs__(self.__cur_pos__)
-                self.__cur_arr_id__, self.__cur_arr_vec__ = self.__next_arr_id__, self.__next_arr_vec__
+                self.__download_next_arrs(self.__cur_pos)
+                self.__cur_arr_id, self.__cur_arr_vec = self.__next_arr_id, self.__next_arr_vec
                 self.__cur_arr_pos__ = 0
-                image_ids, arr = self.__cur_arr_id__[self.__cur_arr_pos__], self.__cur_arr_vec__[self.__cur_arr_pos__]
-                self.__cur_arr_pos__ += 1
-                self.__cur_pos__ += 1
+                image_ids, arr = self.__cur_arr_id[self.__cur_arr_pos], self.__cur_arr_vec[self.__cur_arr_pos]
+                self.__cur_arr_pos += 1
+                self.__cur_pos += 1
                 return image_ids, arr
         else:
             raise StopIteration
 
     def __download_next_arrs__(self, start):
         print(f'Preloading from {start} to {start + self.__chunk_size__}')
-        self.__next_arr_id__, self.__next_arr_vec__ = self.__data_provider__.get(start, self.__chunk_size__)
+        self.__next_arr_id, self.__next_arr_vec = self.__data_provider.get(start, self.__chunk_size)
